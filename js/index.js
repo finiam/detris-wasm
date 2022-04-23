38ac9c8f425747e479e2171b5a4e620837a64d88
Zé Diogo <vianajose.97@gmail.com>
2022-04-23 11:25:55 +0200

    Fixing responsiveness mvp start/end screen

--- js/index.js
+++ js/index.js
@@ -1,70 +1,113 @@
-import("../pkg/index.js")
-  .then((wasm) => {
-    import("../pkg/index_bg.wasm").then((w) => {
-      let game = wasm.Game.new();
+import("../pkg/index.js").then(wasm =>{
+  import ("../pkg/index_bg.wasm").then(w => {
 
-      let CELL_SIZE = Math.min(
-        (window.innerHeight - 100) / 24,
-        (window.innerWidth - 100) / 10
-      );
-      const canvas = document.getElementById("tetris-canvas");
+    let game = wasm.Game.new();
+    let status = 'begin' // begin | play | end
+    let CELL_SIZE = Math.min((window.innerHeight - 100) / 20, (window.innerWidth - 100) / 10);
+    const canvas = document.getElementById('tetris-canvas');
+    canvas.width = 10 * (CELL_SIZE + 2) + 2;
+    canvas.height = 24 * (CELL_SIZE + 2 ) + 2;
+    canvas.style.marginTop = `${CELL_SIZE * -4}px`;
+
+    const ctx = canvas.getContext('2d');
+
+    const colors = {
+      0 : '#000000',
+      1 : '#780737',
+      2 : '#d29708',
+      3 : '#125f03',
+      4 : '#e9e2c7',
+      5 : '#9b6928',
+      6 : '#1c7180',
+      7 : '#569f1b',
+    }
+
+    window.addEventListener('resize', (e) => {
+      CELL_SIZE = Math.min((window.innerHeight - 100) / 20, (window.innerWidth - 100) / 10);
       canvas.width = 10 * (CELL_SIZE + 2) + 2;
-      canvas.height = 24 * (CELL_SIZE + 2) + 2;
+      canvas.height = 24 * (CELL_SIZE + 2 ) + 2;
+      canvas.marginTop = `${CELL_SIZE * -4}px`;
+    });
 
-      const ctx = canvas.getContext("2d");
+    document.addEventListener('keypress', (e) => {
+      switch (e.code) {
+        case 'Space':
+          status = "play"
+          draw();
+          break;
+        case 'KeyA':
+          game.input(8);
+          draw();
+          break;
+        case 'KeyD':
+          game.input(16);
+          draw();
+          break;
+        case 'KeyW':
+          game.input(2);
+          draw();
+          break;
+        case 'KeyS':
+          game.input(4);
+          draw();
+          break;
+        default:
+          break;
+      }
+    });
 
-      const colors = {
-        0: "#000000",
-        1: "#780737",
-        2: "#d29708",
-        3: "#125f03",
-        4: "#e9e2c7",
-        5: "#9b6928",
-        6: "#1c7180",
-        7: "#569f1b",
-      };
-
-      window.addEventListener("resize", (e) => {
-        CELL_SIZE = Math.min(
-          (window.innerHeight - 100) / 24,
-          (window.innerWidth - 100) / 10
-        );
-        canvas.width = 10 * (CELL_SIZE + 2) + 2;
-        canvas.height = 24 * (CELL_SIZE + 2) + 2;
-      });
-
-      document.addEventListener("keypress", (e) => {
-        switch (e.code) {
-          case "KeyA":
-            console.log("Left");
-            game.input(8);
-            draw();
-            break;
-          case "KeyD":
-            game.input(16);
-            console.log("Right");
-            draw();
-            break;
-          case "KeyW":
-            game.input(2);
-            console.log("Up");
-            draw();
-            break;
-          case "KeyS":
-            game.input(4);
-            console.log("Down");
-            draw();
-            break;
-          default:
-            break;
+    function startScreen() {
+      for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+          ctx.fillStyle = `rgb(
+              ${Math.floor(255 - 42.5 * i)},
+              ${Math.floor(255 - 42.5 * j)},
+              0)`;
+          ctx.fillRect(j * 25 + CELL_SIZE * 4, i * 25 + CELL_SIZE * 4, 25, 25);
         }
-      });
+      }
 
-      function draw() {
-        const screenPtr = game.draw();
-        const screen = new Uint8Array(w.memory.buffer, screenPtr, 10 * 24);
-        // console.log(screen)
+      ctx.fillStyle = '#ff0000';
+      ctx.font = '50px serif';
+      ctx.fillText('Hello', 100, 200);
 
+      ctx.font = '20px serif';
+      ctx.fillText('Hit space to start', 100, 400);
+    }
+
+    function endScreen() {
+      for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 6; j++) {
+          ctx.fillStyle = `rgb(
+              ${Math.floor(255 - 42.5 * i)},
+              ${Math.floor(255 - 42.5 * j)},
+              0)`;
+          ctx.fillRect(j * 25 + CELL_SIZE * 4, i * 25 + CELL_SIZE * 4, 25, 25);
+        }
+      }
+
+      ctx.fillStyle = '#ff0000';
+      ctx.font = '50px serif';
+      ctx.fillText('Hello', 100, 200);
+
+      ctx.font = '20px serif';
+      ctx.fillText('Hit space to start', 100, 400);
+    }
+
+    function draw() {
+      if (status === 'begin') {
+        startScreen();
+
+        return;
+      }
+      else if (status === 'end') {
+        endScreen();
+
+        return;
+      }
+
+      const screenPtr = game.draw();
+      const screen = new Uint8Array(w.memory.buffer, screenPtr, 10 * 24);
         ctx.beginPath();
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -97,7 +140,7 @@ import("../pkg/index.js")
         let progress = timestamp - last_tick;
         if (progress > game.tick_delay()) {
           last_tick = timestamp;
-          if (game.tick()) {
+          if (status === 'play' && game.tick()) {
             game.tick_delay = 500;
             game = wasm.Game.new();
           }
