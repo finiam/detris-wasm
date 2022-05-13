3790c579e0544345f4444aae93c2fd87420e23fd
Davide Silva <davidesilva@hey.com>
2022-05-13 16:07:02 +0100

    Show next piece and score (#11)
    * Show next piece and score
    
    Why:
    * To improve the experience of the game
    
    How:
    * Storing the next piece and showing it in the interface
    * Showing the current score in the interface
    * Fixing some visual issues and improve responsiveness
    * Fixing the I piece rotation

--- js/index.js
+++ js/index.js
@@ -4,30 +4,50 @@ import("../pkg/index.js")
       let game = wasm.Game.new();
       let status = "begin"; // begin | play | end
       let score = 0;
+      let nextPiece;
       let inputs = "";
       let last_game_state;
       let CELL_SIZE;
       let canvas;
+      let infoCanvas
 
       function updateSizes() {
-        CELL_SIZE = Math.min(
-          (window.innerHeight - 75) / 20,
-          (window.innerWidth - 30) / 10
-        );
-        const wrapperEl = document.querySelector("#wrapper");
+        const container = document.querySelector("#container");
+        infoCanvas = document.getElementById("info-canvas");
+        scoreCanvas = document.getElementById("score-canvas");
 
-        wrapperEl.style.width = `${10 * (CELL_SIZE + 2) + 8}px`;
-        wrapperEl.style.height = `${20 * (CELL_SIZE + 2) + 6}px`;
+        CELL_SIZE = Math.min(
+          (container.clientHeight - 75) / 20,
+          (container.clientWidth - 50) / 20
+        );
+
+        let negativeMargin = CELL_SIZE * -4;
+        container.style.transform = `translateY(${negativeMargin}px)`;
+        container.style.marginBottom = `${negativeMargin}px`;
+        container.style.setProperty("--top", `${4 * (CELL_SIZE + 2) - 4}px`);
+
+        const wrapperEl = document.querySelector("#wrapper");
+        wrapperEl.style.width = `${10 * (CELL_SIZE + 3)}px`;
+        wrapperEl.style.height = `${20 * (CELL_SIZE + 2) + 7}px`;
         wrapperEl.style.top = `${4 * (CELL_SIZE + 2) + 2}px`;
+
+        const nextPieceWrapperEl = document.querySelector("#next-piece-wrapper");
+        nextPieceWrapperEl.style.width = `${5 * (CELL_SIZE + 2) + 6}px`;
+        nextPieceWrapperEl.style.height = `${8 * (CELL_SIZE + 2) + 6}px`;
+
+        const scoreWrapperEl = document.querySelector("#score-wrapper");
+        scoreWrapperEl.style.width = `${5 * (CELL_SIZE + 2) + 6}px`;
+        scoreWrapperEl.style.height = `${8 * (CELL_SIZE + 2) + 6}px`;
 
         canvas = document.getElementById("tetris-canvas");
         canvas.width = 10 * (CELL_SIZE + 2) + 12;
         canvas.height = 24 * (CELL_SIZE + 2) + 2;
         canvas.style.marginTop = `${-4 * (CELL_SIZE + 2)}px`;
-        let negativeMargin = CELL_SIZE * -4;
-        document.body.style.transform = `translateY(${negativeMargin}px)`;
-        document.body.style.marginBottom = `${negativeMargin}px`;
-        document.body.style.setProperty("--top", `${4 * (CELL_SIZE + 2) - 4}px`);
+
+        infoCanvas.width = 5 * (CELL_SIZE + 2);
+        infoCanvas.height = 8 * (CELL_SIZE + 2);
+        scoreCanvas.width = 5 * (CELL_SIZE + 2);
+        scoreCanvas.height = 8 * (CELL_SIZE + 2);
 
         wrapperEl.style.display = "block";
       }
@@ -35,6 +55,8 @@ import("../pkg/index.js")
       updateSizes();
 
       const ctx = canvas.getContext("2d");
+      const ctxInfo = infoCanvas.getContext("2d");
+      const ctxScore = scoreCanvas.getContext("2d");
 
       const colors = {
         0: "#000000", //  FINIAM    //  RESENDE
@@ -76,11 +98,12 @@ import("../pkg/index.js")
         }
       });
 
-      function paintPieces(color, xx, yy) {
+      function paintPieces(color, xx, yy, nextPiece = false) {
+        const context = nextPiece ? ctxInfo : ctx;
         if (color && color !== "#000000") {
         //   // No Borders
-          ctx.fillStyle = color;
-          ctx.fillRect(
+          context.fillStyle = color;
+          context.fillRect(
             xx,
             yy,
             CELL_SIZE,
@@ -88,17 +111,17 @@ import("../pkg/index.js")
           );
 
           // Only Borders
-          // ctx.strokeStyle = color;
-          // ctx.lineWidth = 4;
-          // ctx.lineJoin = 'round';
-          // ctx.fillStyle = "#000000";
-          // ctx.fillRect(
+          // context.strokeStyle = color;
+          // context.lineWidth = 4;
+          // context.lineJoin = 'round';
+          // context.fillStyle = "#000000";
+          // context.fillRect(
           //   xx,
           //   yy,
           //   CELL_SIZE,
           //   CELL_SIZE
           // );
-          // ctx.strokeRect(
+          // context.strokeRect(
           //   xx,
           //   yy,
           //   CELL_SIZE,
@@ -106,10 +129,10 @@ import("../pkg/index.js")
           // );
 
           // Neon
-          // ctx.shadowColor = 'white';
-          // ctx.shadowBlur = 10;
-          // ctx.fillStyle = color;
-          // ctx.fillRect(
+          // context.shadowColor = 'white';
+          // context.shadowBlur = 10;
+          // context.fillStyle = color;
+          // context.fillRect(
           //   xx,
           //   yy,
           //   CELL_SIZE,
@@ -118,9 +141,9 @@ import("../pkg/index.js")
         }
 
         // With borders / White / Inverted
-        // ctx.fillStyle = color !== "#000000" ? "white" : "#000000";
-        // ctx.fillStyle = color;
-        // ctx.fillRect(
+        // context.fillStyle = color !== "#000000" ? "white" : "#000000";
+        // context.fillStyle = color;
+        // context.fillRect(
         //   xx,
         //   yy,
         //   CELL_SIZE,
@@ -134,7 +157,7 @@ import("../pkg/index.js")
         ctx.textAlign = "center";
 
         ctx.fillStyle = "#5ff2ef";
-        ctx.font = `${canvas.width < 350 ? "30px" : "50px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "20px" : "50px"} 'Press Start 2P'`;
         ctx.fillText(
           "DETRIS",
           canvas.width / 2,
@@ -155,13 +178,13 @@ import("../pkg/index.js")
         ctx.shadowOffsetY = 4;
 
         ctx.fillStyle = "#ffffff";
-        ctx.font = `${canvas.width < 350 ? "16px" : "22px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "13px" : "22px"} 'Press Start 2P'`;
         ctx.fillText("Hit space", canvas.width / 2, canvas.height * 0.45);
         ctx.fillText("to start", canvas.width / 2, canvas.height * 0.45 + 30);
 
         ctx.shadowBlur = 0;
         ctx.fillStyle = "#ffffff";
-        ctx.font = `${canvas.width < 350 ? "13px" : "15px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "10px" : "15px"} 'Press Start 2P'`;
         ctx.fillText("by finiam", canvas.width / 2, canvas.height * 0.66);
 
         ctx.shadowOffsetY = 0;
@@ -195,7 +218,7 @@ import("../pkg/index.js")
         ctx.textAlign = "center";
 
         ctx.fillStyle = "#5ff2ef";
-        ctx.font = `${canvas.width < 350 ? "35px" : "50px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "25px" : "50px"} 'Press Start 2P'`;
         ctx.fillText("DETRIS", canvas.width / 2, canvas.height * 0.3);
 
         ctx.fillStyle = "#ff5050";
@@ -206,17 +229,17 @@ import("../pkg/index.js")
         ctx.shadowOffsetY = 4;
         ctx.fillStyle = "#ffffff";
 
-        ctx.font = `${canvas.width < 350 ? "25px" : "30px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "16px" : "30px"} 'Press Start 2P'`;
         ctx.fillText("GAME OVER", canvas.width / 2, canvas.height * 0.55);
 
-        ctx.font = `${canvas.width < 350 ? "25px" : "30px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "20px" : "30px"} 'Press Start 2P'`;
         ctx.fillText("SCORE", canvas.width / 2, canvas.height * 0.55 + 60);
-        ctx.font = `${canvas.width < 350 ? "30px" : "40px"} 'Press Start 2P'`;
+        ctx.font = `${canvas.width < 300 ? "25px" : "40px"} 'Press Start 2P'`;
         ctx.fillText(score, canvas.width / 2, canvas.height * 0.55 + 120);
 
         ctx.fillStyle = "#ffffff";
-        ctx.font = `${canvas.width < 350 ? "13px" : "15px"} 'Press Start 2P'`;
-        ctx.fillText("by finiam", canvas.width / 2, canvas.height * 0.9);
+        ctx.font = `${canvas.width < 300 ? "10px" : "15px"} 'Press Start 2P'`;
+        ctx.fillText("by finiam", canvas.width / 2, canvas.height * 0.95);
         ctx.shadowOffsetY = 0;
         ctx.shadowOffsetX = 0;
       }
@@ -236,11 +259,14 @@ import("../pkg/index.js")
           return;
         }
 
-        const screenPtr = game.draw();
-        const screen = new Uint8Array(w.memory.buffer, screenPtr, 10 * 24);
         ctx.beginPath();
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctxInfo.clearRect(0, 0, infoCanvas.width, infoCanvas.height);
+        ctxScore.clearRect(0, 0, scoreCanvas.width, scoreCanvas.height);
+
+        const screenPtr = game.draw();
+        const screen = new Uint8Array(w.memory.buffer, screenPtr, 10 * 24);
 
         for (let col = 0; col < 10; col++) {
           for (let row = 4; row < 24; row++) {
@@ -249,6 +275,31 @@ import("../pkg/index.js")
             const xx = col * (CELL_SIZE + 2) + 5;
             const yy = row * (CELL_SIZE + 2) + 5;
             paintPieces(color, xx, yy);
+          }
+        }
+
+        score = game.score();
+        nextPiecePtr = game.next_piece();
+        const nextPiece = Array.from(nextPiecePtr);
+
+        ctxInfo.fillStyle = "white";
+        ctxInfo.font = `${CELL_SIZE * 0.5}px 'Press Start 2P'`;
+        ctxInfo.fillText("NEXT", CELL_SIZE, CELL_SIZE * 2);
+
+        ctxScore.textAlign = "center";
+        ctxScore.fillStyle = "white";
+        ctxScore.font = `${CELL_SIZE * 0.5}px 'Press Start 2P'`;
+        ctxScore.fillText("SCORE", scoreCanvas.width / 2, CELL_SIZE * 2);
+        ctxScore.font = "20px 'Press Start 2P";
+        ctxScore.fillText(score, scoreCanvas.width / 2, CELL_SIZE * 2 + 60);
+
+        for (let col = 0; col < 3; col++) {
+          for (let row = 0; row < 4; row++) {
+            const idx = col + row * 3;
+            const color = colors[nextPiece[idx]];
+            const xx = CELL_SIZE + col * (CELL_SIZE + 2);
+            const yy = CELL_SIZE * 3 + row * (CELL_SIZE + 2);
+            paintPieces(color, xx, yy, true);
           }
         }
 
